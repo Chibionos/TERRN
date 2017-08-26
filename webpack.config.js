5a62d30bede6086aea17870a855912bc27b700f7
John <tiptopcode@gmail.com>
2017-08-26 17:26:33 -0500

    First Commit

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+    include: [
+        path.resolve(__dirname, "src/client/"),
+        path.resolve(__dirname, "src/common/")
+    ],
+    output: {
+        path: path.resolve(__dirname, 'dist/build/'),  
+        filename: 'bundle.js',
+        publicPath: '/dist/build/'
+    },
+    resolve: {
+        // Add `.ts` and `.tsx` as a resolvable extension.
+        extensions: ['.webpack.js', '.web.js', '.ts', '.tsx', '.js']
+    },
+    module: {
+        loaders: [
+            // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
+            { test: /\.tsx?$/, loader: 'ts-loader' }
+        ]
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+          'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+          compress: { warnings: false },
+          mangle: true,
+          sourcemap: false,
+          beautify: false,
+          dead_code: true
+        })
+      ]
+};
